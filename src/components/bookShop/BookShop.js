88cfc3313c299db6e043aec25fa21764854a6d51
Sveta Sadykova <sadykova.o5o2@gmail.com>
2024-01-28 23:56:53 +0600

    GenresSection1

--- src/components/bookShop/BookShop.js
+++ src/components/bookShop/BookShop.js
@@ -1,22 +1,26 @@
 import React from "react";
+import Genres from "./genres/Genres";
 
 export default function BookShop() {
   return (
-    <section
-      id="bookShop"
-      style={{
-        display: "flex",
-        justifyContent: "center",
-        alignItems: "center",
-        height: "80vh",
-      }}
-    >
-      <div className="container">
-        <div className="text-center">
-          <h1 className="text-[#fff] text-[48px]">Welcome to our Bookshop</h1>
-          <p>A place where you can get all the books you want!</p>
+    <div>
+      <section
+        id="bookShop"
+        style={{
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+          height: "80vh",
+        }}
+      >
+        <div className="container">
+          <div className="text-center">
+            <h1 className="text-[#fff] text-[48px]">Welcome to our Bookshop</h1>
+            <p>A place where you can get all the books you want!</p>
+          </div>
         </div>
-      </div>
-    </section>
+      </section>
+      <Genres />
+    </div>
   );
 }
---
+++ src/components/bookShop/genres/Genres.js
@@ -0,0 +1,41 @@
+import React from "react";
+import card from "../../images/card.png";
+import card1 from "../../images/card (1).png";
+import card2 from "../../images/card (2).png";
+import card3 from "../../images/card (3).png";
+import card4 from "../../images/card (4).png";
+import card5 from "../../images/card (5).png";
+export default function Genres() {
+  return (
+    <div>
+      <div className="container">
+        <div className=" justify-center ">
+          <div className="flex justify-between">
+            <h1
+              style={{
+                fontSize: "48px",
+                fontWeight: "500",
+                color: "#19272E",
+              }}
+            >
+              Genres
+            </h1>
+            <p
+              style={{ fontSize: "18px", fontWeight: "500", color: "#818EEA" }}
+            >
+              View all
+            </p>
+          </div>
+          <div className="flex justify-center gap-14">
+            <img src={card} alt="" />
+            <img src={card1} alt="" />
+            <img src={card2} alt="" />
+            <img src={card3} alt="" />
+            <img src={card4} alt="" />
+            <img src={card5} alt="" />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
