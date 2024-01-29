06fe67f947df7e02e94fd49234542b19519263ca
Meerim004 <sultanova90747>
2024-01-29 19:13:11 +0600

    welcome

--- src/components/bookShop/genres/Genres.js
+++ src/components/bookShop/genres/Genres.js
@@ -6,44 +6,44 @@ import card3 from "../../images/card (3).png";
 import card4 from "../../images/card (4).png";
 import card5 from "../../images/card (5).png";
 export default function Genres() {
-  return (
-    <div>
+   return (
       <div className="container">
-<<<<<<< HEAD
-        <div style={{ marginTop: "140px" }} className=" justify-center">
-=======
-        <div className=" justify-center pt-[40px]">
->>>>>>> 0ec59a1f54fcce34ee7c42bb4a1f1c1f32b16396
-          <div className="flex justify-between">
-            <h1
-              style={{
-                fontSize: "48px",
-                fontWeight: "500",
-                color: "#19272E",
-                marginTop: "-25px",
-              }}
-            >
-              Genres
-            </h1>
-            <p
-              style={{ fontSize: "18px", fontWeight: "500", color: "#818EEA" }}
-            >
-              View all
-            </p>
-          </div>
-          <div
-            style={{ marginTop: "60px" }}
-            className="flex justify-center gap-14"
-          >
-            <img src={card} alt="" />
-            <img src={card1} alt="" />
-            <img src={card2} alt="" />
-            <img src={card3} alt="" />
-            <img src={card4} alt="" />
-            <img src={card5} alt="" />
-          </div>
-        </div>
+         <div style={{ marginTop: "140px" }} className=" justify-center">
+            <div className=" justify-center pt-[40px]">
+               <div className="flex justify-between">
+                  <h1
+                     style={{
+                        fontSize: "48px",
+                        fontWeight: "500",
+                        color: "#19272E",
+                        marginTop: "-25px",
+                     }}
+                  >
+                     Genres
+                  </h1>
+                  <p
+                     style={{
+                        fontSize: "18px",
+                        fontWeight: "500",
+                        color: "#818EEA",
+                     }}
+                  >
+                     View all
+                  </p>
+               </div>
+               <div
+                  style={{ marginTop: "60px" }}
+                  className="flex justify-center gap-14"
+               >
+                  <img src={card} alt="" />
+                  <img src={card1} alt="" />
+                  <img src={card2} alt="" />
+                  <img src={card3} alt="" />
+                  <img src={card4} alt="" />
+                  <img src={card5} alt="" />
+               </div>
+            </div>
+         </div>
       </div>
-    </div>
-  );
+   );
 }
